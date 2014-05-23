e2067cd4f72f84a45d5f330052b2ba8fbfc5fd24
enchained <ann.enchained@gmail.com>
2014-05-23 22:01:53 +0400

    test page with data from array instead of API

--- js/app.js
+++ js/app.js
@@ -1,16 +1,220 @@
 'use strict';
 
 
-(function(){
-	var app = angular.module('flickr-search', [ ]);
-	
+(function() {
+	var app = angular.module('flickr-search', []);
+	var pictures = [
+        {
+            title: 'Photo Walk Blue Hour at the Trump Tower',
+            img: 'http://totallyfrickinawesome.com/wp-content/uploads/2014/03/a94f9ddbaa4ec8ea829dc4a6ddac7b73-150x150.jpg',
+            flickr: 'https://flic.kr/p/bp6BcX',
+        },
+        {
+            title: 'Photo Walk Blue Hour at the Trump Tower',
+            img: 'http://totallyfrickinawesome.com/wp-content/uploads/2014/03/a94f9ddbaa4ec8ea829dc4a6ddac7b73-150x150.jpg',
+            flickr: 'https://flic.kr/p/bp6BcX',
+        },
+        {
+            title: 'Photo Walk Blue Hour at the Trump Tower',
+            img: 'http://totallyfrickinawesome.com/wp-content/uploads/2014/03/a94f9ddbaa4ec8ea829dc4a6ddac7b73-150x150.jpg',
+            flickr: 'https://flic.kr/p/bp6BcX',
+        },
+        {
+            title: 'Photo Walk Blue Hour at the Trump Tower',
+            img: 'http://totallyfrickinawesome.com/wp-content/uploads/2014/03/a94f9ddbaa4ec8ea829dc4a6ddac7b73-150x150.jpg',
+            flickr: 'https://flic.kr/p/bp6BcX',
+        },
+        {
+            title: 'Photo Walk Blue Hour at the Trump Tower',
+            img: 'http://totallyfrickinawesome.com/wp-content/uploads/2014/03/a94f9ddbaa4ec8ea829dc4a6ddac7b73-150x150.jpg',
+            flickr: 'https://flic.kr/p/bp6BcX',
+        },
+        {
+            title: 'Photo Walk Blue Hour at the Trump Tower',
+            img: 'http://totallyfrickinawesome.com/wp-content/uploads/2014/03/a94f9ddbaa4ec8ea829dc4a6ddac7b73-150x150.jpg',
+            flickr: 'https://flic.kr/p/bp6BcX',
+        },
+        {
+            title: 'Photo Walk Blue Hour at the Trump Tower',
+            img: 'http://totallyfrickinawesome.com/wp-content/uploads/2014/03/a94f9ddbaa4ec8ea829dc4a6ddac7b73-150x150.jpg',
+            flickr: 'https://flic.kr/p/bp6BcX',
+        },
+        {
+            title: 'Photo Walk Blue Hour at the Trump Tower',
+            img: 'http://totallyfrickinawesome.com/wp-content/uploads/2014/03/a94f9ddbaa4ec8ea829dc4a6ddac7b73-150x150.jpg',
+            flickr: 'https://flic.kr/p/bp6BcX',
+        },
+        {
+            title: 'Photo Walk Blue Hour at the Trump Tower',
+            img: 'http://totallyfrickinawesome.com/wp-content/uploads/2014/03/a94f9ddbaa4ec8ea829dc4a6ddac7b73-150x150.jpg',
+            flickr: 'https://flic.kr/p/bp6BcX',
+        },
+        {
+            title: 'Photo Walk Blue Hour at the Trump Tower',
+            img: 'http://totallyfrickinawesome.com/wp-content/uploads/2014/03/a94f9ddbaa4ec8ea829dc4a6ddac7b73-150x150.jpg',
+            flickr: 'https://flic.kr/p/bp6BcX',
+        },
+        {
+            title: 'Photo Walk Blue Hour at the Trump Tower',
+            img: 'http://totallyfrickinawesome.com/wp-content/uploads/2014/03/a94f9ddbaa4ec8ea829dc4a6ddac7b73-150x150.jpg',
+            flickr: 'https://flic.kr/p/bp6BcX',
+        },
+        {
+            title: 'Photo Walk Blue Hour at the Trump Tower',
+            img: 'http://totallyfrickinawesome.com/wp-content/uploads/2014/03/a94f9ddbaa4ec8ea829dc4a6ddac7b73-150x150.jpg',
+            flickr: 'https://flic.kr/p/bp6BcX',
+        },
+        {
+            title: 'Photo Walk Blue Hour at the Trump Tower',
+            img: 'http://totallyfrickinawesome.com/wp-content/uploads/2014/03/a94f9ddbaa4ec8ea829dc4a6ddac7b73-150x150.jpg',
+            flickr: 'https://flic.kr/p/bp6BcX',
+        },
+        {
+            title: 'Photo Walk Blue Hour at the Trump Tower',
+            img: 'http://totallyfrickinawesome.com/wp-content/uploads/2014/03/a94f9ddbaa4ec8ea829dc4a6ddac7b73-150x150.jpg',
+            flickr: 'https://flic.kr/p/bp6BcX',
+        },
+        {
+            title: 'Photo Walk Blue Hour at the Trump Tower',
+            img: 'http://totallyfrickinawesome.com/wp-content/uploads/2014/03/a94f9ddbaa4ec8ea829dc4a6ddac7b73-150x150.jpg',
+            flickr: 'https://flic.kr/p/bp6BcX',
+        },
+        {
+            title: 'Photo Walk Blue Hour at the Trump Tower',
+            img: 'http://totallyfrickinawesome.com/wp-content/uploads/2014/03/a94f9ddbaa4ec8ea829dc4a6ddac7b73-150x150.jpg',
+            flickr: 'https://flic.kr/p/bp6BcX',
+        },
+        {
+            title: 'Photo Walk Blue Hour at the Trump Tower',
+            img: 'http://totallyfrickinawesome.com/wp-content/uploads/2014/03/a94f9ddbaa4ec8ea829dc4a6ddac7b73-150x150.jpg',
+            flickr: 'https://flic.kr/p/bp6BcX',
+        },
+        {
+            title: 'Photo Walk Blue Hour at the Trump Tower',
+            img: 'http://totallyfrickinawesome.com/wp-content/uploads/2014/03/a94f9ddbaa4ec8ea829dc4a6ddac7b73-150x150.jpg',
+            flickr: 'https://flic.kr/p/bp6BcX',
+        },
+        {
+            title: 'Photo Walk Blue Hour at the Trump Tower',
+            img: 'http://totallyfrickinawesome.com/wp-content/uploads/2014/03/a94f9ddbaa4ec8ea829dc4a6ddac7b73-150x150.jpg',
+            flickr: 'https://flic.kr/p/bp6BcX',
+        },
+        {
+            title: 'Photo Walk Blue Hour at the Trump Tower',
+            img: 'http://totallyfrickinawesome.com/wp-content/uploads/2014/03/a94f9ddbaa4ec8ea829dc4a6ddac7b73-150x150.jpg',
+            flickr: 'https://flic.kr/p/bp6BcX',
+        },
+        {
+            title: 'Photo Walk Blue Hour at the Trump Tower',
+            img: 'http://totallyfrickinawesome.com/wp-content/uploads/2014/03/a94f9ddbaa4ec8ea829dc4a6ddac7b73-150x150.jpg',
+            flickr: 'https://flic.kr/p/bp6BcX',
+        },
+        {
+            title: 'Photo Walk Blue Hour at the Trump Tower',
+            img: 'http://totallyfrickinawesome.com/wp-content/uploads/2014/03/a94f9ddbaa4ec8ea829dc4a6ddac7b73-150x150.jpg',
+            flickr: 'https://flic.kr/p/bp6BcX',
+        },
+        {
+            title: 'Photo Walk Blue Hour at the Trump Tower',
+            img: 'http://totallyfrickinawesome.com/wp-content/uploads/2014/03/a94f9ddbaa4ec8ea829dc4a6ddac7b73-150x150.jpg',
+            flickr: 'https://flic.kr/p/bp6BcX',
+        },
+        {
+            title: 'Photo Walk Blue Hour at the Trump Tower',
+            img: 'http://totallyfrickinawesome.com/wp-content/uploads/2014/03/a94f9ddbaa4ec8ea829dc4a6ddac7b73-150x150.jpg',
+            flickr: 'https://flic.kr/p/bp6BcX',
+        },
+        {
+            title: 'Photo Walk Blue Hour at the Trump Tower',
+            img: 'http://totallyfrickinawesome.com/wp-content/uploads/2014/03/a94f9ddbaa4ec8ea829dc4a6ddac7b73-150x150.jpg',
+            flickr: 'https://flic.kr/p/bp6BcX',
+        },
+        {
+            title: 'Photo Walk Blue Hour at the Trump Tower',
+            img: 'http://totallyfrickinawesome.com/wp-content/uploads/2014/03/a94f9ddbaa4ec8ea829dc4a6ddac7b73-150x150.jpg',
+            flickr: 'https://flic.kr/p/bp6BcX',
+        },
+        {
+            title: 'Photo Walk Blue Hour at the Trump Tower',
+            img: 'http://totallyfrickinawesome.com/wp-content/uploads/2014/03/a94f9ddbaa4ec8ea829dc4a6ddac7b73-150x150.jpg',
+            flickr: 'https://flic.kr/p/bp6BcX',
+        },
+        {
+            title: 'Photo Walk Blue Hour at the Trump Tower',
+            img: 'http://totallyfrickinawesome.com/wp-content/uploads/2014/03/a94f9ddbaa4ec8ea829dc4a6ddac7b73-150x150.jpg',
+            flickr: 'https://flic.kr/p/bp6BcX',
+        },
+        {
+            title: 'Photo Walk Blue Hour at the Trump Tower',
+            img: 'http://totallyfrickinawesome.com/wp-content/uploads/2014/03/a94f9ddbaa4ec8ea829dc4a6ddac7b73-150x150.jpg',
+            flickr: 'https://flic.kr/p/bp6BcX',
+        },
+        {
+            title: 'Photo Walk Blue Hour at the Trump Tower',
+            img: 'http://totallyfrickinawesome.com/wp-content/uploads/2014/03/a94f9ddbaa4ec8ea829dc4a6ddac7b73-150x150.jpg',
+            flickr: 'https://flic.kr/p/bp6BcX',
+        },
+    ];
+
+    app.filter('offset', function() {
+        return function(input, start) {
+            start = parseInt(start, 10);
+            return input.slice(start);
+      };
+    });    
+        
 	app.controller('SearchController', function(){
-		this.product = pic1;
+		this.products = pictures;
+        this.itemsPerPage = 12;
+        this.currentPage = 0;
+
+        this.range = function() {
+            var rangeSize = 5;
+            var ret = [];
+            var start;
+
+            start = this.currentPage;
+            if ( start > this.pageCount()-rangeSize ) {
+              start = this.pageCount()-rangeSize+1;
+            }
+
+            for (var i=start; i<start+rangeSize; i++) {
+              ret.push(i);
+            }
+            return ret;
+        };
+
+
+        this.prevPage = function() {
+            if (this.currentPage > 0) {
+                this.currentPage--;
+            }
+        };
+
+        this.prevPageDisabled = function() {
+            return this.currentPage === 0 ? "disabled" : "";
+        };
+
+        this.pageCount = function() {
+            return Math.ceil(this.products.length/this.itemsPerPage)-1;
+        };
+
+        this.nextPage = function() {
+            if (this.currentPage < this.pageCount()) {
+                this.currentPage++;
+            }
+        };
+
+        this.nextPageDisabled = function() {
+            return this.currentPage === this.pageCount() ? "disabled" : "";
+        };
+
+        this.setPage = function(n) {
+            this.currentPage = n;
+        };
+
 	});
 
-	var pic1= {
-		title: 'Photo Walk Blue Hour at the Trump Tower',
-		img: 'http://totallyfrickinawesome.com/wp-content/uploads/2014/03/a94f9ddbaa4ec8ea829dc4a6ddac7b73-150x150.jpg',
-		flickr: 'https://flic.kr/p/bp6BcX',
-	}
+	
+        
 })();
